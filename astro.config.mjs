e93307d486cbe317b65bceb8ae1b189812120364
DESKTOP-NA482UJ\ADM <chabeto72@hotmail.com>
2026-04-07 20:40:55 -0500

    update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://goldmodelsstudio.com',
   integrations: [],
   devToolbar: {
     enabled: false
