c8a8336265fba3d27b0a61b64fb0b8ed517cf4b2
DESKTOP-NA482UJ\ADM <chabeto72@hotmail.com>
2026-04-27 20:23:37 -0500

    Add Google Analytics 4 support with PUBLIC_GA_MEASUREMENT_ID configuration

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="astro/client" />
 
 declare const Astro: import("astro").AstroGlobal;
+
+interface ImportMetaEnv {
+	readonly PUBLIC_GA_MEASUREMENT_ID?: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
